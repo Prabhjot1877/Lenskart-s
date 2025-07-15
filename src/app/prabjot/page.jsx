25a4b6e3ea1158d135247588ad719cdf281a6736
Mahi singh <aka.mahisingh@gmail.com>
2025-07-15 12:09:19 +0530

    EVERYTHING WORKS

--- src/app/prabjot/page.jsx
+++ src/app/prabjot/page.jsx
@@ -181,7 +181,7 @@ export default function AddProduct() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 py-8 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-50 py-8 px-4 sm:px-6 lg:px-8 pt-28">
       <div className="max-w-4xl mx-auto">
         {/* Header */}
         <motion.div
